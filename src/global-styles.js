99b1464f81ad72041c899e6c45afaab194da2c32
Van Dao NGUYEN <van-dao.nguyen@etudiant.univ-reims.fr>
2020-09-27 21:31:17 +0200

    Footer added

---
+++ src/global-styles.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    html, body {
+        font-family: 'Helvetica Neue', Arial, Helvetica, sans-serif;
+        background-color: black;
+        color : #FFF;
+        font-size: 16px;
+    }
+`;
